4c04d005f46bda4261df02657d1dafaf3ef85b1f
Marcos Solano <114309325+MarcosSolano1977@users.noreply.github.com>
2023-04-30 20:19:27 -0600

    actualizacion de nombre

--- Consumo_WCFv5 Todo BLL/Consumo_WCFv2 Todo BLL/PL/Teclados.aspx.cs
+++ Consumo_WCFv5 Todo BLL/Consumo_WCFv2 Todo BLL/PL/Teclados.aspx.cs
@@ -46,7 +46,7 @@ namespace PL
 
         protected void btn_Guardar_Click(object sender, EventArgs e)
         {
-            Obj_Teclados_DAL.iId_Teclado = Convert.ToInt32(txt_IdTeclado.Text.Trim());
+            Obj_Teclados_DAL.iId_Teclado = Convert.ToInt32(txtaFiltProd.Text.Trim());
             Obj_Teclados_DAL.sMarca_Teclado = txt_MarcaTeclado.Text.Trim();
             Obj_Teclados_DAL.sModelo_Teclado = txt_ModeloTeclado.Text.Trim();
             Obj_Teclados_DAL.sTipo_Teclado = txt_TipoTeclado.Text.Trim();
